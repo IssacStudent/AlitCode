ba657089ad61f2a2fe5b18997cdb0899c862de12
IssacStudent <ynwang0125@gmail.com>
2023-03-21 22:07:38 +0800

    移动0，暴力解法&双指针（移动非0，而不是0）

---
+++ src/main/java/com/wyn/top100/MoveZeroes.java
@@ -0,0 +1,46 @@
+package com.wyn.top100;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ *
+ * 请注意 ，必须在不复制数组的情况下原地对数组进行操作。
+ * @author Yining Wang
+ * @date 2023年3月21日21:05:29
+ * @since <pre>2023/03/21</pre>
+ */
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums) {
+            if (num != 0) {
+                list.add(num);
+            }
+        }
+        for (int i = 0; i < list.size(); i++) {
+            nums[i] = list.get(i);
+        }
+        for (int i = list.size(); i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+
+    public void moveZeroes2(int[] nums) {
+        int n = nums.length, left = 0, right = 0;
+        while (right < n) {
+            if (nums[right] != 0) {
+                swap(nums, left, right);
+                left++;
+            }
+            right++;
+        }
+    }
+
+    public void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
+}
