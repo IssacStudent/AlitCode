cb3102f9acd9b4c88035447042a1da338b9c8e74
IssacStudent <ynwang0125@gmail.com>
2023-03-22 15:55:07 +0800

    买卖股票的最佳时间II

--- src/main/java/com/wyn/top100/MaxProfit.java
+++ src/main/java/com/wyn/top100/MaxProfit.java
@@ -1,10 +1,13 @@
 package com.wyn.top100;
 
-import java.util.HashMap;
-
+/**
+ * 买卖股票的最佳时间I、II
+ * @author Yining Wang
+ * @date 2023年3月22日15:36:53
+ * @since <pre>2023/03/22</pre>
+ */
 public class MaxProfit {
-    public int maxProfit(int[] prices) {
-        int l = 0, r = prices.length - 1;
+    public int maxProfit1(int[] prices) {
         int result = 0;
         int min = Integer.MAX_VALUE;
         for (int i = 0; i < prices.length; i++) {
@@ -12,5 +15,26 @@ public class MaxProfit {
             result = Math.max(result, prices[i] - min);
         }
         return result;
+    }
+
+    /**
+     * 思路：一段一段的计算利润，并且只计算递增的，如果递减就直接跳过
+     * @param prices
+     * @return
+     */
+    public int maxProfit2(int[] prices) {
+        int result = 0;
+        for (int i = 1, mark = 0;i< prices.length; i++, mark++){
+            if (prices[i] >= prices[mark]) {
+                result += (prices[i] - prices[mark]);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{7,1,5,3,6,4};
+        MaxProfit maxProfit = new MaxProfit();
+        System.out.println(maxProfit.maxProfit2(nums));
     }
 }
